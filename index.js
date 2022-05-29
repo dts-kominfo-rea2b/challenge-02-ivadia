f0f2b7a9b5c4e2958add5304705cbe5a5bb57206
Ivadia <ivadia2401@gmail.com>
2022-05-29 09:28:27 +0800

    done challenge 2 Logic Flow

--- index.js
+++ index.js
@@ -23,19 +23,27 @@ function lakukanLooping(arrPegawai) {
 
       Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
   */
-  let hasilLooping = null;
+      let hasilLooping = [];
+      dataYangAkanDilooping.forEach((pegawai) => {
+          hasilLooping.push(`${pegawai.namaDepan} ${pegawai.namaBelakang}`);
+      });
 
   /*
     TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
       yang berisi jumlah pria dari masing masing pegawai
   */
-  let jumlahPria = null;
+      let jumlahPria = 0;
+      dataYangAkanDilooping.forEach((pegawai) => {
+          pegawai.jenisKelamin == "M" ? jumlahPria++ : "";
+      });
 
   /*
     TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
       yang berisi jumlah wanita dari masing masing pegawai
   */
-  let jumlahWanita = null;
+      let jumlahWanita = 0;
+      dataYangAkanDilooping.forEach((pegawai) => {
+          pegawai.jenisKelamin == "F" ? jumlahWanita++ : "";
 
   /*
     TODO 4: Buatlah sebuah variabel bernama "komentar"
@@ -48,7 +56,14 @@ function lakukanLooping(arrPegawai) {
       Apabila imbang, maka komentar akan berisi:
         "Jumlah Pria dan Wanita berimbang"
   */
-  let komentar = null;
+        let komentar = null;
+        if (jumlahPria > jumlahWanita) {
+            komentar = "Jumlah Pria lebih banyak dari Wanita";
+        } else if (jumlahPria < jumlahWanita) {
+            komentar = "Jumlah Wanita lebih banyak dari Pria";
+        } else {
+            komentar = "Jumlah Pria dan Wanita berimbang";
+        }
 
   // ! JANGAN DIMODIFIKASI
   return {
